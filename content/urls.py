ab5c96a0f66b0bde6421f97d10c80f327d82c1ae
UNIQUE ADHIKARI <adhikariunique56@gmail.com>
2025-01-08 16:51:44 +0545

    defining project structure

--- content/urls.py
+++ content/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path
+from .views.main_views import home, create_blog, edit_blog, single_blog
+from .views.auth_views import login, register
 
 urlpatterns = [
-    path('', views.index)
+    path('', home),
+    path('register/', register),
+    path('login/', login),
+    path('create/', create_blog),
+    path('edit/', edit_blog),
+    path('single/', single_blog),
 ]
+
