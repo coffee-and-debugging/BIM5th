6a28f0f3ccfbae9b2f049e45bb5f0af1fae9656d
UNIQUE ADHIKARI <adhikariunique56@gmail.com>
2025-01-23 12:04:32 +0545

    Admin Register

--- content/models.py
+++ content/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blogs(models.Model):
+    title= models.CharField(max_length=50)
+    subtitle= models.CharField(blank=True, null=True, max_length=100)
+    image= models.ImageField(upload_to="images")
+    description= models.TextField()
+    created_at= models.DateTimeField(auto_now_add=True)
--- content/views/main_views.py
+++ content/views/main_views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from ..models import Blogs
 
 def home(request):
     return render(request, 'main/home.html')
 
-def create_blog(request):
+def create_blog(request):   
+    if request.method=="POST":
+        title= request.POST.get('title')
+        subtitle= request.POST.get('subtitle')
+        description= request.POST.get('description')
+        
+        blog= Blogs(title=title, subtitle=subtitle, description=description)
+        blog.save()
+        
+        return render(request, 'main/create_blog.html')       
+        
+        
+
     return render(request, 'main/create_blog.html')
 
 def edit_blog(request):
