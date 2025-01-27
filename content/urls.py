a988e29e86a2f82db46f5ed29536d01e83aea30a
UNIQUE ADHIKARI <adhikariunique56@gmail.com>
2025-01-27 18:09:01 +0545

    Register Page

--- content/urls.py
+++ content/urls.py
@@ -4,9 +4,9 @@ from .views.main_views import home, create_blog, edit_blog, single_blog
 from .views.auth_views import login, register
 
 urlpatterns = [
-    path('', home),
-    path('register/', register),
-    path('login/', login),
+    path('', home, name="home"),
+    path('register/', register, name="register"),
+    path('login/', login, name="login"),
     path('create/', create_blog),
     path('edit/', edit_blog),
     path('single/', single_blog),
