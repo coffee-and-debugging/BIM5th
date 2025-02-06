64c56ad5d6c5b6ada330ba9dbfeca286a37fec96
UNIQUE ADHIKARI <adhikariunique56@gmail.com>
2025-02-06 12:02:49 +0545

    Login logic

--- content/views/auth_views.py
+++ content/views/auth_views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as Login
 
 def login(request):
+    if request.method=="POST":
+        username= request.POST.get("username")
+        password= request.POST.get("password")
+        
+        user= authenticate(request, username=username, password=password)
+        
+        if user is None:
+            return redirect("login")
+        else:
+            Login(request, user)
+            return redirect("home")
+          
     return render(request, 'auth/login.html')
 
 def register(request):
