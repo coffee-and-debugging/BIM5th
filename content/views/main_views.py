a62400134a5f829241d25459dfe74a43f9cd1062
UNIQUE ADHIKARI <adhikariunique56@gmail.com>
2025-02-09 12:01:07 +0545

    image handling

--- content/views/main_views.py
+++ content/views/main_views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from ..models import Blogs
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'main/home.html')
 
+
+@login_required
 def create_blog(request):   
     if request.method=="POST":
         title= request.POST.get('title')
         subtitle= request.POST.get('subtitle')
         description= request.POST.get('description')
+        image= request.FILES.get("image")
         
-        blog= Blogs(title=title, subtitle=subtitle, description=description)
+        blog= Blogs(title=title, subtitle=subtitle, description=description, image=image)
         blog.save()
         
         return render(request, 'main/create_blog.html')       
